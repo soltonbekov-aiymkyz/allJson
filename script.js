30455afffec04fe0d5b0b5f09817dedfc95039c7
Your Name <you@example.com>
2021-02-21 23:46:38 -0800

    dkfkvf

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-fetch("./js.json")
+/*fetch("./js.json")
 .then(res => res.js.json())
-.then(data => console.log(data))
+.then(data => console.log(data))*/
+
+let button = document.querySelector('button');
+let inputName = document.querySelector('#name');
+let inputMessage = document.querySelector('#message');
+let inputColor = document.querySelector('#color');
+
+button.addEventListener('click', function () {
+    let div = document.createElement('div');
+    let spanName = document.createElement('span');
+    let spanMessage = document.createElement('span');
+
+    spanName.innerText = inputName.value + ": ";
+    spanMessage.innerText = inputMessage.value;
+    spanName.style.color = inputColor.value;
+    spanName.style.fontWeight = 'bold';
+
+    div.append(spanName, spanMessage);
+    document.body.append(div);
+
+    fetch("./https://live-chat-d7603-default-rtdb.firebaseio.com/chat.json" , {
+        method: "POST",
+        body: JSON.stringify({
+            name: inputName.value,
+            message: inputMessage.value,
+            color: inputColor.value,
+        })
+    })
+})
+
+
+
+
